8339435605ff1fce41b1e6ee01d4a68961d4e4b4
Petros <petoyanpetros97@gmail.com>
2022-04-07 19:28:46 +0400

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ const elvenShieldRecipe = {
     refinedMoonstone: 4,
 
 }
+const some = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4
+}
 
 console.log(elvenShieldRecipe)
+console.log(some, 11111111111111111)
